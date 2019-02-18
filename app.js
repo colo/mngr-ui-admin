26215aa84ed997d153274c1d283251aa76e2bf01
Luciano Bueno <glamero@gmail.com>
2019-02-18 13:11:40 -0300

    bump

--- app.js
+++ app.js
@@ -194,7 +194,9 @@ var MyApp = new Class({
 		this.parent(options);//override default options
 
 		let io = require("socket.io")(server, {
-			transports: ['websocket', 'polling']
+			transports: ['websocket', 'polling'],
+			pingInterval: 5000,
+  		pingTimeout: 5000
 		})
 		io.use(sharedsession(this.session, {
 		    autoSave:true
